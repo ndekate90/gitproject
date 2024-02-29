2477ff804c27d0571762c1fa69c679d84ee000e9
ndekate90 <161463795+ndekate90@users.noreply.github.com>
2024-02-29 22:06:28 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res)
 	{
-		res.write('this is my node js project');
+		res.write('this is my node js project and pls share this');
 		res.end();
 
 	}).listen(3000);
